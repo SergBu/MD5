d905a71be2c18b7660811019702559707a640714
s.budnikov <sergei.budnikov@smartseeds.ru>
2024-06-09 17:07:11 +0300

    dfgdfg

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var input = "hello";
+var output = "";
+
+var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+
+using (var md5 = System.Security.Cryptography.MD5.Create())
+{
+    var hashBytes = md5.ComputeHash(inputBytes);
+    output = Convert.ToHexString(hashBytes);
+}
+
+Console.WriteLine($"MD5 hash is {output}");
+
+var hash = MD5.MD5.Calculate(inputBytes);
+
+Console.WriteLine($"MD5 hash is {output}");
